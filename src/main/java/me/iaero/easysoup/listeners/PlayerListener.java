547321c5925424f8033fad90e35deb064ee72dfb
aerouk <xcraaze@gmail.com>
2014-03-29 15:13:18 +0000

    Removed some needless brackets.

--- src/main/java/me/iaero/easysoup/listeners/PlayerListener.java
+++ src/main/java/me/iaero/easysoup/listeners/PlayerListener.java
@@ -14,7 +14,7 @@ public class PlayerListener implements Listener
     {
         Player player = event.getPlayer();
 
-        if (((event.getAction() == Action.RIGHT_CLICK_BLOCK) || (event.getAction() == Action.RIGHT_CLICK_AIR)) && (player.getItemInHand().getType() == Material.MUSHROOM_SOUP) && (player.getHealth() != 20.0) && (player.getHealth() + 1.0 <= 20.0))
+        if ((event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR) && player.getItemInHand().getType() == Material.MUSHROOM_SOUP && player.getHealth() != 20.0 && player.getHealth() + 1.0 <= 20.0)
         {
             event.setCancelled(true);
             player.setHealth(player.getHealth() + 7.0 > player.getMaxHealth() ? player.getMaxHealth() : player.getHealth() + 7.0);
